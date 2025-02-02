c1f6b98e21de425e6cbecd7eb91f889ef9408fcc
klabser <klabser@gmail.com>
2025-02-02 22:11:22 +0300

    connet ui

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("pypyp")
+from PyQt5 import uic, QtWidgets, QtCore
+
+
+
+class TableWindow(QtWidgets.QMainWindow):
+    """
+    основное окно
+    """
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('TableWindow.ui', self)
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = TableWindow()
+    window.show()
+
+    app.exec_()
